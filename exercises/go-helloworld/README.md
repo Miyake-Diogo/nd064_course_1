6a8099d254c7322dbc39ccd346add25308fa0ec0
Diogo Miyake <diogo.miyake@xpi.com.br>
2021-06-09 08:37:00 -0300

    Update files of exercises

--- exercises/go-helloworld/README.md
+++ exercises/go-helloworld/README.md
@@ -12,3 +12,29 @@ go run main.go
 ```
 
 Access the application on: http://127.0.0.1:6111/
+
+
+# To use Dockerfile
+
+You can pull my image from dockerhub
+`docker pull miyakediogo/go-helloworld:v1.0.0`
+
+To mount this image see Dockerfile in repo. 
+I use this commands: 
+```bash
+# build the image
+docker build -t go-helloworld .
+
+# tag the image
+docker tag go-helloworld miyakediogo/go-helloworld:v1.0.0
+
+# push the image
+docker push miyakediogo/go-helloworld:v1.0.0
+```
+
+To run I use:
+```bash
+docker run -p 6111:6111 miyakediogo/go-helloworld:v1.0.0  
+```
+
+After this go to browser and write `localhost:6111` to see helloworld webpage.
--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Status request successfull')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+
+    app.logger.info('Metrics request successfull')
+    return response
+
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
+    
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
